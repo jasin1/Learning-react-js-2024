34c752fb6bfaaa3f859d49b37d247cd41912cf2f
jasin1 <jasin.tairaidrissi@gmail.com>
2024-09-10 07:28:11 +0200

    added Routing

---
+++ Learning-react-js-2024/src/pages/Colors/Colors.jsx
@@ -0,0 +1,11 @@
+import './colors.css';
+
+function Colors(){
+  return(
+    <main>
+      <h1>Guess the color code</h1>
+    </main>
+  );
+}
+
+export default Colors;
---
+++ Learning-react-js-2024/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home(){
+  return(
+    <main>
+      <h1>Homepage</h1>
+    </main>
+  );
+
+}
+
+export default Home;
