e5fa129a8821c6606770c86cfea38b5497b7a4b6
jasin1 <jasin.tairaidrissi@gmail.com>
2024-09-10 07:44:41 +0200

    adding css

--- Learning-react-js-2024/src/pages/Colors/Colors.jsx
+++ Learning-react-js-2024/src/pages/Colors/Colors.jsx
@@ -4,6 +4,15 @@ function Colors(){
   return(
     <main>
       <h1>Guess the color code</h1>
+      <div className="colors-wrapper">
+        <div className="colors-view"></div>
+        <div className="btns-wrapper">
+          <button>hex</button>
+          <button>hex</button>
+          <button>hex</button>
+        </div>
+
+      </div>
     </main>
   );
 }
